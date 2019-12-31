60129779534f714bc98c5336266795542bbc7c83
HyeranShin <hyeran9712@naver.com>
2019-12-31 15:56:13 +0900

    가게 목록 api 작성

--- eatgo-api/src/main/java/kr/co/fastcampus/eatgo/domain/Restaurant.java
+++ eatgo-api/src/main/java/kr/co/fastcampus/eatgo/domain/Restaurant.java
@@ -2,12 +2,18 @@ package kr.co.fastcampus.eatgo.domain;
 
 public class Restaurant {
 
+    private final Long id;
     private final String name;
     private final String address;
 
-    public Restaurant(String name, String address) {
+    public Restaurant(Long id, String name, String address) {
+        this.id = id;
         this.name = name;
         this.address = address;
+    }
+
+    public Long getId() {
+        return id;
     }
 
     public String getName() {
--- eatgo-api/src/test/java/kr/co/fastcampus/eatgo/domain/RestaurantTest.java
+++ eatgo-api/src/test/java/kr/co/fastcampus/eatgo/domain/RestaurantTest.java
@@ -9,15 +9,16 @@ public class RestaurantTest {
 
     @Test
     public void creation() {
-        Restaurant restaurant = new Restaurant("Bob zip", "Seoul");
+        Restaurant restaurant = new Restaurant(1004L, "Bob zip", "Seoul");
 
+        assertThat(restaurant.getId(), is(1004L));
         assertThat(restaurant.getName(), is("Bob zip"));
         assertThat(restaurant.getAddress(), is("Seoul"));
     }
 
     @Test
     public void informain() {
-        Restaurant restaurant = new Restaurant("Bob zip", "Seoul");
+        Restaurant restaurant = new Restaurant(1004L, "Bob zip", "Seoul");
 
         assertThat(restaurant.getInformation(), is("Bob zip in Seoul"));
     }
