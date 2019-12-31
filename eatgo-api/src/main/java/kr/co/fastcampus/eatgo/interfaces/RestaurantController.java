2b5ffcd54acaaa8bb2ad525af8a6c483f04ffa62
HyeranShin <hyeran9712@naver.com>
2019-12-31 17:14:21 +0900

    가게 상세 api 작성

--- eatgo-api/src/main/java/kr/co/fastcampus/eatgo/interfaces/RestaurantController.java
+++ eatgo-api/src/main/java/kr/co/fastcampus/eatgo/interfaces/RestaurantController.java
@@ -1,23 +1,30 @@
 package kr.co.fastcampus.eatgo.interfaces;
 
 import kr.co.fastcampus.eatgo.domain.Restaurant;
+import kr.co.fastcampus.eatgo.domain.RestaurantRepository;
+import org.apache.catalina.util.ErrorPageSupport;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class RestaurantController {
 
+    private RestaurantRepository repository = new RestaurantRepository();
+
     @GetMapping("/restaurants")
     public List<Restaurant> list() {
-        List<Restaurant> restaurants = new ArrayList<>();
-
-        Restaurant restaurant = new Restaurant(1004L, "Bob zip", "Seoul");
-
-        restaurants.add(restaurant);
+        List<Restaurant> restaurants = repository.findAll();
 
         return restaurants;
+    }
+
+    @GetMapping("/restaurants/{id}")
+    public Restaurant detail(@PathVariable("id") Long id) {
+        Restaurant restaurant = repository.findById(id);
+
+        return restaurant;
     }
 }
