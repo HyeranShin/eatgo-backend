844433f834bbe0fd4d38acc5fec8483bf778dd48
HyeranShin <hyeran9712@naver.com>
2020-01-03 03:53:35 +0900

    메뉴 수정, 삭제 api 작성

--- eatgo-api/src/main/java/kr/co/fastcampus/eatgo/domain/MenuItem.java
+++ eatgo-api/src/main/java/kr/co/fastcampus/eatgo/domain/MenuItem.java
@@ -1,10 +1,12 @@
 package kr.co.fastcampus.eatgo.domain;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.*;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Transient;
 
 @Entity
 @Getter
@@ -21,4 +23,8 @@ public class MenuItem {
     private Long restaurantId;
 
     private String name;
+
+    @Transient  // DB에 넣지 않는다.
+    @JsonInclude(JsonInclude.Include.NON_DEFAULT)
+    private boolean destroy;
 }
