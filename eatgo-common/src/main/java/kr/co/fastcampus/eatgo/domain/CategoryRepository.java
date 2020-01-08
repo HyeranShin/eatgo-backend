693e8365238a60b11efc9ff440cc9f48cb51aceb
HyeranShin <hyeran9712@naver.com>
2020-01-08 16:09:51 +0900

    카테고리 목록, 추가 api 작성

---
+++ eatgo-common/src/main/java/kr/co/fastcampus/eatgo/domain/CategoryRepository.java
@@ -0,0 +1,10 @@
+package kr.co.fastcampus.eatgo.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface CategoryRepository extends CrudRepository<Category, Long> {
+
+    List<Category> findAll();
+}
---
+++ eatgo-customer-api/src/test/java/kr/co/fastcampus/eatgo/application/CategoryServiceTest.java
@@ -0,0 +1,46 @@
+package kr.co.fastcampus.eatgo.application;
+
+import kr.co.fastcampus.eatgo.application.CategoryService;
+import kr.co.fastcampus.eatgo.domain.Category;
+import kr.co.fastcampus.eatgo.domain.CategoryRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+
+public class CategoryServiceTest {
+
+    private CategoryService categoryService;
+
+    @Mock
+    private CategoryRepository categoryRepository;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+
+        categoryService = new CategoryService(categoryRepository);
+    }
+
+    @Test
+    public void getCategories() {
+        List<Category> mockCategories = new ArrayList<>();
+        mockCategories.add(Category.builder().name("Korean Food").build());
+
+        given(categoryRepository.findAll()).willReturn(mockCategories);
+
+        List<Category> categories = categoryService.getCategories();
+
+        Category category = categories.get(0);
+        assertThat(category.getName(), is("Korean Food"));
+    }
+}
