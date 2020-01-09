6b62bf6827eeb77a94b002a4d7444c58f3b5d0e7
HyeranShin <hyeran9712@naver.com>
2020-01-09 23:34:21 +0900

    회원가입 api 작성

--- eatgo-common/src/main/java/kr/co/fastcampus/eatgo/domain/User.java
+++ eatgo-common/src/main/java/kr/co/fastcampus/eatgo/domain/User.java
@@ -31,6 +31,8 @@ public class User {
     @NotNull
     private Long level;
 
+    private String password;
+
     public boolean isAdmin() {
         return level >= 100;
     }
---
+++ eatgo-customer-api/src/test/java/kr/co/fastcampus/eatgo/application/UserServiceTest.java
@@ -0,0 +1,55 @@
+package kr.co.fastcampus.eatgo.application;
+
+import kr.co.fastcampus.eatgo.domain.User;
+import kr.co.fastcampus.eatgo.domain.UserRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+
+public class UserServiceTest {
+
+    private UserService userService;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+
+        userService = new UserService(userRepository);
+    }
+
+    @Test
+    public void registerUser() {
+        String email = "tester@example.com";
+        String name = "Tester";
+        String password = "test";
+
+        userService.registerUser(email, name, password);
+
+        verify(userRepository).save(any());
+    }
+
+    @Test(expected = EmailExistedException.class)
+    public void registerUserWithExistedEmail() {
+        String email = "tester@example.com";
+        String name = "Tester";
+        String password = "test";
+
+        User user = User.builder().build();
+        given(userRepository.findByEmail(email)).willReturn(Optional.of(user));
+
+        userService.registerUser(email, name, password);
+
+        verify(userRepository, never()).save(any());
+    }
+}
