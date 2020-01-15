bc3a92572a8186c0e8190e7076e6b92f6079a587
HyeranShin <hyeran9712@naver.com>
2020-01-15 15:02:51 +0900

    restaurant-api module 생성, 테이블 예약 목록 api 작성

--- eatgo-common/src/main/java/kr/co/fastcampus/eatgo/domain/ReservationRepository.java
+++ eatgo-common/src/main/java/kr/co/fastcampus/eatgo/domain/ReservationRepository.java
@@ -2,5 +2,9 @@ package kr.co.fastcampus.eatgo.domain;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface ReservationRepository extends CrudRepository<Reservation, Long> {
+
+    List<Reservation> findAllByRestaurantId(Long restaurantId);
 }
---
+++ eatgo-restaurant-api/src/test/java/kr/co/fastcampus/eatgo/EatgoRestaurantApiApplicationTest.java
@@ -0,0 +1,13 @@
+package kr.co.fastcampus.eatgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class EatgoRestaurantApiApplicationTest {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
