9dd8fa0c15556a1afb28794c236e731c48e4645e
HyeranShin <hyeran9712@naver.com>
2020-01-15 14:13:50 +0900

    테이블 예약 api 작성

---
+++ eatgo-common/src/main/java/kr/co/fastcampus/eatgo/domain/Reservation.java
@@ -0,0 +1,41 @@
+package kr.co.fastcampus.eatgo.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Reservation {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private Long restaurantId;
+
+    private Long userId;
+
+    private String name;
+
+    @NotEmpty
+    private String date;
+
+    @NotEmpty
+    private String time;
+
+    @NotNull
+    private Integer partySize;
+}
---
+++ eatgo-common/src/main/java/kr/co/fastcampus/eatgo/domain/ReservationRepository.java
@@ -0,0 +1,6 @@
+package kr.co.fastcampus.eatgo.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ReservationRepository extends CrudRepository<Reservation, Long> {
+}
